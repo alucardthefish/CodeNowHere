6bb9c7070eb2a8db8be5e88a5692934e17402157
alucardthefish <sergiop18@gmail.com>
2020-01-20 01:11:33 -0500

    Fix bash instructions

--- build_local.sh
+++ build_local.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "building binary locally in /build directory..."
-g++ src/Main.cpp src/CodeNowHere.cpp -o build/cnhtest
-echo "binary created: build/cnhtest"
+mkdir build
+echo "build directory created"
+g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp -o build/cnhtest
+echo "binary build: cnhtest"
 exit 0
