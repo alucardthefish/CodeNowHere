c9173b9f48d2ed205d6bb6d8f572d56c81b38078
alucardthefish <sergiop18@gmail.com>
2020-01-31 21:35:18 -0500

    Update the local build script with docopt.cpp libraries

--- build_local.sh
+++ build_local.sh
@@ -2,6 +2,6 @@
 echo "building binary locally in /build directory..."
 mkdir build
 echo "build directory created"
-g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp -o build/cnhtest
+g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp -o build/cnhtestopt
 echo "binary build: cnhtest"
 exit 0
