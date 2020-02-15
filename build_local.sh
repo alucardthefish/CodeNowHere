604dfddd52dfca3351c3e19bb051e3a204196a86
alucardthefish <sergiop18@gmail.com>
2020-02-14 22:25:04 -0500

    Update script add linking to ManyFileBehavior.cpp

--- build_local.sh
+++ build_local.sh
@@ -2,6 +2,6 @@
 echo "building binary locally in /build directory..."
 mkdir build
 echo "build directory created"
-g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp src/OnlyFileBehavior.cpp src/ICommandBehavior.cpp src/Helper.cpp -o build/cnh
+g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp src/OnlyFileBehavior.cpp src/ManyFileBehavior.cpp src/ICommandBehavior.cpp src/Helper.cpp -o build/cnh
 echo "binary build: cnhtest"
 exit 0
