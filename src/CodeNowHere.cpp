79f61525400a45355ea1b01157c9949535711199
alucardthefish <sergiop18@gmail.com>
2020-02-16 17:41:36 -0500

    Update controller
    Conditional for creation of single code file uses argument this now.

--- src/CodeNowHere.cpp
+++ src/CodeNowHere.cpp
@@ -14,10 +14,8 @@ CodeNowHere::CodeNowHere(map<string, docopt::value> input) {
 
 void CodeNowHere::Controller() {
     //ICommandBehavior *iCB;
-    if (arguments["<filename>"].isString()) {
-         if(!arguments["<filename>"].asString().empty()) {
-             iCB = new OnlyFileBehavior();
-         }
+    if (arguments["this"].asBool()){
+        iCB = new OnlyFileBehavior();
     } else if (arguments["these"].asBool()) {
         iCB = new ManyFileBehavior();
     }
