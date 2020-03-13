7ebebdf2e3c240ae8b0d17abdf370148e6fef47e
alucardthefish <sergiop18@gmail.com>
2020-03-13 01:48:09 -0500

    Typo in message

--- build_local.sh
+++ build_local.sh
@@ -3,5 +3,5 @@ echo "building binary locally in /build directory..."
 mkdir build
 echo "build directory created"
 g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp src/OnlyFileBehavior.cpp src/ManyFileBehavior.cpp src/BunchFileBehavior.cpp src/ICommandBehavior.cpp src/Helper.cpp -o build/cnh
-echo "binary build: cnhtest"
+echo "binary build: cnh"
 exit 0
