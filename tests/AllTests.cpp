3222f9e36c9c50886665fcf3fc0f9fca85a07493
alucardthefish <sergiop18@gmail.com>
2020-03-16 21:26:55 -0500

    Link submodule gtest
    Compile and link the googletest library locally to the project.
    Update the configuration files to accept tests dir.
    Create the Makefile.am file for describing the initial test behavior.

---
+++ tests/AllTests.cpp
@@ -0,0 +1,19 @@
+// **************************************************************************** 
+// File: AllTests.cpp 
+// Author: alucardthefish 
+// Created: Mon Mar 16 21:18:51 2020 
+// Description: Main Program for running unit tests 
+// **************************************************************************** 
+
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char * argv[]) {
+    std::cout << "Test Runner (Start)" << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
