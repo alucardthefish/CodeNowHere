304fb5aeeddd7aa0e6eb722ad2ace49d744c3a1c
alucardthefish <sergiop18@gmail.com>
2020-03-18 19:52:47 -0500

    Add new files to sources section for compiling and linking

--- build_local.sh
+++ build_local.sh
@@ -2,6 +2,6 @@
 echo "building binary locally in /build directory..."
 mkdir build
 echo "build directory created"
-g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp src/OnlyFileBehavior.cpp src/ManyFileBehavior.cpp src/BunchFileBehavior.cpp src/ICommandBehavior.cpp src/Helper.cpp -o build/cnh
+g++ -std=gnu++11 Main.cpp src/CodeNowHere.cpp src/docopt.cpp src/OnlyFileBehavior.cpp src/cnh_structs.cpp src/ManyFileBehavior.cpp src/BunchFileBehavior.cpp src/ICommandBehavior.cpp src/Helper.cpp -o build/cnh
 echo "binary build: cnh"
 exit 0
